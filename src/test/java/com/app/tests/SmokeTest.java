776cbadae9a9561f47d29e92ca16bf7382e6052e
ikbol009@gmail.com <ikbol009@gmail.com>
2018-03-19 17:43:14 -0400

    added and tested smoketest, made corrections

--- src/test/java/com/app/tests/SmokeTest.java
+++ src/test/java/com/app/tests/SmokeTest.java
@@ -21,60 +21,56 @@ StatistaPage statistaPage = new StatistaPage();
 	public void Test() {
 		
 		assertTrue(BrowserUtils.isAt());
-		
 		BrowserUtils.searchStatistaButton(statistaPage.homepageTextSearch, "Panera Bread");
-		
-		assertEquals("Panera Bred",statistaPage.searchBoxText.getAttribute("value"));
-		assertTrue(statistaPage.searchBoxText.isDisplayed());
-		
+		assertEquals("Panera Bread",statistaPage.searchBoxText.getAttribute("value"), 
+				"Verifying search box text");
+		assertTrue(statistaPage.searchBoxText.isDisplayed(), 
+				"Verifying text inside the search box isDisplayed");
 		String name="Statistics";
 //		assertTrue(statistaPage.optionsStatic.isDisplayed()) ;
-		System.out.println(BrowserUtils.getTopicNameLists(statistaPage.topicNameLists, name));
-		assertTrue(BrowserUtils.getTopicNameLists(statistaPage.topicNameLists,name).equals(name));
+		assertTrue(BrowserUtils.getTopicNameLists(statistaPage.topicNameLists,name).equals(name), 
+				"Verifying TopicNameLists \"Statistics\"");
 		
 		name="Forecasts & surveys";
 //		assertTrue(statistaPage.optionsInfos.isDisplayed());
-		System.out.println(BrowserUtils.getTopicNameLists(statistaPage.topicNameLists, name));
-		assertTrue(BrowserUtils.getTopicNameLists(statistaPage.topicNameLists,name).equals(name));
+		assertTrue(BrowserUtils.getTopicNameLists(statistaPage.topicNameLists,name).equals(name), 
+				"Verifying TopicNameLists \"Forecasts & surveys\"");
 		
 		name="Infographics";
 //		assertTrue(statistaPage.optionsInfos.isDisplayed());
-		System.out.println(BrowserUtils.getTopicNameLists(statistaPage.topicNameLists, name));
-		assertTrue(BrowserUtils.getTopicNameLists(statistaPage.topicNameLists,name).equals(name));
+		assertTrue(BrowserUtils.getTopicNameLists(statistaPage.topicNameLists,name).equals(name), 
+				"Verifying TopicNameLists \"Infographics\"");
 		
 		name="Topics";
 //		assertTrue(statistaPage.optionsTopics.isDisplayed());
-		System.out.println(BrowserUtils.getTopicNameLists(statistaPage.topicNameLists, name));
-		assertTrue(BrowserUtils.getTopicNameLists(statistaPage.topicNameLists,name).equals(name));
+		assertTrue(BrowserUtils.getTopicNameLists(statistaPage.topicNameLists,name).equals(name), 
+				"Verifying TopicNameLists \"Topics\"");
 		
 		BrowserUtils.getTopicListsAndClick(statistaPage.topicLists, "Statistics");
-
 		BrowserUtils.waitFor(3);
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Statistics"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Statistics"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Forecasts & surveys"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Forecasts & surveys"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Infographics"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Infographics"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Topics"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Topics"));
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Statistics"),
+				"Statista check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Forecasts & surveys"),
+				"Forecasts & surveys check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Infographics"),
+				"Infographics check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Topics"),
+				"Topics check box");
 		
-		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Dossiers"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Dossiers"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Statista studies"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Statista studies"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Industry & country reports"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Industry & country reports"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Additional studies"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Additional studies"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Digital Markets"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Digital Markets"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Consumer Markets"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Consumer Markets"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Companies"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Companies"));
-
-					
+		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Dossiers"),
+				"Dossiers uncheck box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Statista studies"),
+				"Statista studies uncheck box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Industry & country reports"),
+				"Industry & country reports uncheck box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Additional studies"),
+				"Additional studies uncheck box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Digital Markets"),
+				"Digital Markets uncheck box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Consumer Markets"),
+				"Consumer Markets uncheck box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Companies"),
+				"Companies uncheck box");				
 	}
 
 }
--- src/test/java/com/app/tests/Statista.java
+++ src/test/java/com/app/tests/Statista.java
@@ -14,7 +14,7 @@ public class Statista extends TestBase{
 	
 	StatistaPage statistaPage = new StatistaPage();
 	//1,7,13,19
-//	@Test(priority=1)
+	@Test(priority=1)
 	public void testST01() {
 		
 		String title="• Statista - The Statistics Portal for Market Data, Market Research and Market Studies";
@@ -22,30 +22,24 @@ public class Statista extends TestBase{
 		String dataSearch="Panera Bread";
 		
 	//1
-		assertTrue(driver.getTitle().equals(title));
-		System.out.println(driver.getTitle().equals(title));
+		assertTrue(driver.getTitle().equals(title), "Verifying Title");
 	//2
-		assertEquals(driver.getCurrentUrl(), url);
-		System.out.println(driver.getCurrentUrl().equals(url));
+		assertEquals(driver.getCurrentUrl(), url, "Verifying URL");
 	//3
 		BrowserUtils.searchStatistaButton(statistaPage.homepageTextSearch, dataSearch);
 	//4
-		assertEquals(statistaPage.searchBoxText.getAttribute("value"), dataSearch);
-		System.out.println(statistaPage.searchBoxText.getAttribute("value").equals(dataSearch));
-		BrowserUtils.waitForPageToLoad(3);
+		assertEquals(statistaPage.searchBoxText.getAttribute("value"), dataSearch,"Checking search box text");
+//		BrowserUtils.waitForPageToLoad(3);
 	}
 	
-//	@Test(priority=7)
+	@Test(priority=7)
 	public void testST07() {
-		
-		BrowserUtils.waitForPageToLoad(3);
 		
 		String alrtMessage="Unfortunately, no content could be found. Please check your spelling, try an alternative term or expand your search to the Statista archive.";
 		String narrowMessage="Didn’t find what you were looking for?";
 		
 	//1
-		assertTrue(driver.findElement(By.id("statistaLogo")).isDisplayed());
-		System.out.println(driver.findElement(By.id("statistaLogo")).isDisplayed());
+		assertTrue(driver.findElement(By.id("statistaLogo")).isDisplayed(), "Verifying \"statistaLogo\" isDisplayed 1");
 	//2
 		statistaPage.homepageTextSearch.sendKeys("Hello");
 		BrowserUtils.waitFor(5);
@@ -53,66 +47,53 @@ public class Statista extends TestBase{
 	//3
 		BrowserUtils.searchStatistaButton(statistaPage.homepageTextSearch, "%%, **, !!");
 	//4
-		assertTrue(driver.findElement(By.id("statistaLogo")).isDisplayed());
-		System.out.println(driver.findElement(By.id("statistaLogo")).isDisplayed());
+		assertTrue(driver.findElement(By.id("statistaLogo")).isDisplayed(), "Verifying \"statistaLogo\" isDisplayed 2");
 	//5
-		assertTrue(statistaPage.alertMessage.isDisplayed());
-		assertTrue(statistaPage.alertMessage.getText().contains(alrtMessage));
-		System.out.println(statistaPage.alertMessage.isDisplayed());
-		
+		assertTrue(statistaPage.alertMessage.isDisplayed(), "Verifying alert message isDisplayed");
+		assertTrue(statistaPage.alertMessage.getText().contains(alrtMessage), "Verifying alert message text");
 	//6
 		statistaPage.searchButton.click();
-		
-		assertTrue(statistaPage.suggestionMessage.isDisplayed());
-		assertTrue(statistaPage.suggestionMessage.getText().contains(narrowMessage));
-		System.out.println(statistaPage.suggestionMessage.isDisplayed());
-
+		assertTrue(statistaPage.suggestionMessage.isDisplayed(), "Verifying narrow message isDisplayed");
+		assertTrue(statistaPage.suggestionMessage.getText().contains(narrowMessage), "Verifying narrow message text");	
 	}
-	
-	
-	
-//	@Test (priority=13)
+		
+	@Test(priority=13)
 	public void testST013() {
 		
 		String dataSearch="Panera Bread";
 		String firstResultName="Americans preferring Panera Bread in the U.S. in 2016 and 2017, by gender";
 	//1
-		assertTrue(BrowserUtils.isAt());
+		assertTrue(BrowserUtils.isAt(), "Verifying HomePage Title");
 	//2
 		BrowserUtils.searchStatistaButton(statistaPage.homepageTextSearch, dataSearch);
-		assertEquals(statistaPage.searchBoxText.getAttribute("value"), dataSearch);
-		System.out.println(statistaPage.searchBoxText.getAttribute("value").equals(dataSearch));
+		assertEquals(statistaPage.searchBoxText.getAttribute("value"), dataSearch, "Checking search box text");
 	//3
-		assertTrue(driver.getTitle().equals("• Search | Statista"));
-		System.out.println(driver.getTitle().equals("• Search | Statista"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Statistics"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Statistics"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Forecasts & surveys"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Forecasts & surveys"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Infographics"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Infographics"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Topics"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Topics"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Dossiers"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Dossiers"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Statista studies"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Statista studies"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Industry & country reports"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Industry & country reports"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Additional studies"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Additional studies"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Digital Markets"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Digital Markets"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Consumer Markets"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Consumer Markets"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Companies"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Companies"));
-		
-		assertTrue(statistaPage.firstResultText.getText().equals(firstResultName));
-		System.out.println(statistaPage.firstResultText.getText());
+		assertTrue(driver.getTitle().equals("• Search | Statista"), "Verifying Result Title");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Statistics"),
+				"Statista check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Forecasts & surveys"),
+				"Forecasts & surveys check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Infographics"),
+				"Infographics check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Topics"),
+				"Topics check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Dossiers"),
+				"Dossiers check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Statista studies"),
+				"Statista studies check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Industry & country reports"),
+				"Industry & country reports check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Additional studies"),
+				"Additional studies check box");	
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Digital Markets"),
+				"Digital Markets check box");	
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Consumer Markets"),
+				"Consumer Markets check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Companies"),
+				"Companies uncheck box");
+		assertTrue(statistaPage.firstResultText.getText().equals(firstResultName), "Compairing First Results text");
 	//4
 		int resultNum1=Integer.parseInt(statistaPage.resultNum.getText().substring(1, statistaPage.resultNum.getText().length()-1));
-		System.out.println(resultNum1);
 	//5
 		BrowserUtils.checkAndUncheck("Dossiers");
 		BrowserUtils.checkAndUncheck("Statista studies");
@@ -120,18 +101,14 @@ public class Statista extends TestBase{
 		BrowserUtils.checkAndUncheck("Additional studies");
 		statistaPage.refreshSearchButton.click();
 		BrowserUtils.waitFor(3);
+		
 		int resultNum2=Integer.parseInt(statistaPage.resultNum.getText().substring(1, statistaPage.resultNum.getText().length()-1));
-		System.out.println(resultNum2);
-		System.out.println(BrowserUtils.getTopicNamesResultNum("Dossiers")
-			    +BrowserUtils.getTopicNamesResultNum("Statista studies")
-			    +BrowserUtils.getTopicNamesResultNum("Industry & country reports")
-			    +BrowserUtils.getTopicNamesResultNum("Additional studies")
-			    +resultNum2+" "+resultNum1);
+	
 		assertEquals(BrowserUtils.getTopicNamesResultNum("Dossiers")
 				    +BrowserUtils.getTopicNamesResultNum("Statista studies")
 				    +BrowserUtils.getTopicNamesResultNum("Industry & country reports")
 				    +BrowserUtils.getTopicNamesResultNum("Additional studies")
-				    +resultNum2, resultNum1);
+				    +resultNum2, resultNum1,"Verifying TopicNamesResultNum 1 == ");
 		
 	}
 	
@@ -143,83 +120,76 @@ public class Statista extends TestBase{
 		String dataSearch="cat and dog";
 		String firstResultName="Dog/cat food import volume in South Korea from U.S. 2000-2017";
 	//1
-		assertTrue(BrowserUtils.isAt());
-		assertTrue(driver.getCurrentUrl().equals("https://www.statista.com/"));
+		assertTrue(BrowserUtils.isAt(),"Verifying Title");
+		assertTrue(driver.getCurrentUrl().equals("https://www.statista.com/"),"Verifyung URL");
 	//2
 		BrowserUtils.searchStatistaButton(statistaPage.homepageTextSearch, dataSearch);
-		assertEquals(statistaPage.searchBoxText.getAttribute("value"), dataSearch);
-		System.out.println(statistaPage.searchBoxText.getAttribute("value").equals(dataSearch));
+		assertEquals(statistaPage.searchBoxText.getAttribute("value"), dataSearch
+				,"Checking search box text ");
 	//3
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Statistics"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Statistics"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Forecasts & surveys"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Forecasts & surveys"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Infographics"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Infographics"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Topics"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Topics"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Dossiers"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Dossiers"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Statista studies"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Statista studies"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Industry & country reports"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Industry & country reports"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Additional studies"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Additional studies"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Digital Markets"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Digital Markets"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Consumer Markets"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Consumer Markets"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Companies"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Companies"));
-//		assertTrue(statistaPage.firstResultText.getText().equals(firstResultName));
-		System.out.println(statistaPage.firstResultText.getText());	
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Statistics"),
+				"Statista check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Forecasts & surveys"),
+				"Forecasts & surveys check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Infographics"),
+				"Infographics check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Topics"),
+				"Topics check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Dossiers"),
+				"Dossiers check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Statista studies"),
+				"Statista studies check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Industry & country reports"),
+				"Industry & country reports check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Additional studies"),
+				"Additional studies check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Digital Markets"),
+				"Digital Markets check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Consumer Markets"),
+				"Consumer Markets check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Companies"),
+				"Companies uncheck box");
+//		assertTrue(statistaPage.firstResultText.getText().equals(firstResultName),
+//				"Compairing First Results text 1");
 	//4
 		int resultNum1=Integer.parseInt(statistaPage.resultNum.getText().substring(1, statistaPage.resultNum.getText().length()-1));
 	//5
 		BrowserUtils.getTopicListsAndClick(statistaPage.topicLists, "Statistics");
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Statistics"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Statistics"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Forecasts & surveys"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Forecasts & surveys"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Infographics"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Infographics"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Topics"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Topics"));
+		BrowserUtils.waitFor(3);
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Statistics"),
+				"Statistics check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Forecasts & surveys"),
+				"Forecasts & surveys check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Infographics"),
+				"Infographics check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsChecked(statistaPage.topicCheckBoxes, "Topics"),
+				"Topics check box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Dossiers"),
+				"Dossiers uncheck box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Statista studies"),
+				"Statista studies uncheck box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Industry & country reports"),
+				"Industry & country reports uncheck box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Additional studies"),
+				"Additional studies uncheck box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Digital Markets"),
+				"Digital Markets uncheck box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Consumer Markets"),
+				"Consumer Markets uncheck box");
+		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Companies"),
+				"Companies uncheck box");
 		
-		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Dossiers"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Dossiers"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Statista studies"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Statista studies"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Industry & country reports"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Industry & country reports"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Additional studies"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Additional studies"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Digital Markets"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Digital Markets"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Consumer Markets"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Consumer Markets"));
-		assertTrue(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Companies"));
-		System.out.println(BrowserUtils.verifingCheckBoxIsUnchecked(statistaPage.topicCheckBoxes, "Companies"));
 		int resultNum2=Integer.parseInt(statistaPage.resultNum.getText().substring(1, statistaPage.resultNum.getText().length()-1));
-		System.out.println(resultNum2);
-		System.out.println(BrowserUtils.getTopicNamesResultNum("Statistics")
-			    +BrowserUtils.getTopicNamesResultNum("Forecasts & surveys")
-			    +BrowserUtils.getTopicNamesResultNum("Infographics")
-			    +BrowserUtils.getTopicNamesResultNum("Topics")
-			    +" "+resultNum2);
+		
 		assertEquals(BrowserUtils.getTopicNamesResultNum("Statistics")
 				    +BrowserUtils.getTopicNamesResultNum("Forecasts & surveys")
 				    +BrowserUtils.getTopicNamesResultNum("Infographics")
 				    +BrowserUtils.getTopicNamesResultNum("Topics"),
-				    resultNum2);
-//		assertTrue(statistaPage.firstResultText.getText().equals(firstResultName));
-		System.out.println(statistaPage.firstResultText.getText());
+				    resultNum2,"Verifying TopicNamesResultNum 2 ==");
+//		assertTrue(statistaPage.firstResultText.getText().equals(firstResultName),
+//				"Comparing First result text 2");
 	//6
-//		assertTrue(resultNum1>resultNum2);
-		System.out.println(resultNum1>resultNum2);
-		
-		
+		assertTrue(resultNum1>resultNum2, "Compairing results num < >");
 	}
 
 }
