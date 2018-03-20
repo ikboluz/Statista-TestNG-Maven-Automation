6f4a35f80253190a9994515811e2453fac9610d1
aayad72 <33642806+aayad72@users.noreply.github.com>
2018-03-20 17:14:47 -0400

    uploading 4 test cases

--- src/test/java/com/app/pages/StatistaPage.java
+++ src/test/java/com/app/pages/StatistaPage.java
@@ -5,37 +5,36 @@ import java.util.List;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
 import org.openqa.selenium.support.PageFactory;
 
 import com.app.utilities.Driver;
 
 public class StatistaPage {
-	
+
 	private WebDriver driver;
-	
-	 public StatistaPage() {
-		 this.driver=Driver.getDriver();
-		 PageFactory.initElements(driver, this);
-	 }
-	
-	 @FindBy(id="homepageSearch")
-	 public WebElement homepageTextSearch;
-	 
-	 @FindBy(className="button button--primary colOne")
-	 public WebElement homepageSearchButton;
-	 
-	 @FindBy(id="q")
-	 public WebElement searchBoxText;
-	 
-	 @FindBy(xpath="//label[@class='entitiy__label']")
-	 public List<WebElement> topicNameLists;
-	 
-	 @FindBy(xpath="//h3[@class='entitiy__headline cursor-pointer']")
-	 public List<WebElement> topicLists;
-	 
-	 @FindBy(xpath="//input[@class='entitiy__checkbox']")
-	 public List<WebElement> topicCheckBoxes;
-	 
-	
+	@FindBy(id = "homepageSearch")
+	public WebElement homepageTextSearch;
+
+	@FindBy(className = "button button--primary colOne")
+	public WebElement homepageSearchButton;
+
+	@FindBy(id = "q")
+	public WebElement searchBoxText;
+
+	@FindBy(xpath = "//label[@class='entitiy__label']")
+	public List<WebElement> topicNameLists;
+
+	@FindBy(xpath = "//h3[@class='entitiy__headline cursor-pointer']")
+	public List<WebElement> topicLists;
+
+	@FindBy(xpath = "//input[@class='entitiy__checkbox']")
+	public List<WebElement> topicCheckBoxes;
+
+	@FindBy(xpath = "//input[@value='Statista Search']")
+	public WebElement StatistaSearchSubmit;
+
+	@FindBy(className = "ui-menu-item-wrapper")
+	public List<WebElement> DynamicDropSearch;
 
 }
